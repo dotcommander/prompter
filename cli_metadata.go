6d0119b072ce129c380e8da0ae62471b7d45c988
Gary Blankenship <gary@blankenship.me>
2026-09-24 13:26:05 -0400

    chore(release): bump version to 0.5.0

--- cli_metadata.go
+++ cli_metadata.go
@@ -155,7 +155,7 @@ func sensitiveURLParameter(key string) bool {
 }
 
 // AppVersion is the baseline semver for prompter releases.
-const AppVersion = "0.4.0"
+const AppVersion = "0.5.0"
 
 func getVersionString() string {
 	info, ok := debug.ReadBuildInfo()
